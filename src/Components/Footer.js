f7f9eaf0c6efead86f97952e17f55b6dcd9ac22e
Ricardo <SH_Ricardo@hotmail.com>
2022-11-03 11:35:00 -0300

    footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -7,31 +7,123 @@ const Footer = () => {
     <div className={theme}>
       <div className="footer">
         <div className="row">
-          <div className="col-4 ">
-            <div className="logo ">
+          <div className="col-4 text-center">
+            <div className="logo pt-3 ">
               <small className="mx-5 fs-3">
                 <small className="logoR">{"RR"}</small>
                 {"code"}
               </small>
             </div>
-            <small>
-              Dame la oportunidad, aun que sea un desarrollador junior me sobran
-              ganas de aprender , se que puedo aportar mucho. Dame ese desafio
-              que estoy buscando.&nbsp;&nbsp;&nbsp;&nbsp;{"=)"}
+            <small className="parrafo ">
+              Dame la oportunidad, aun que sea un desarrollador junior
+              <br /> me sobran ganas de aprender , se que puedo aportar mucho.
+              <br />
+              Dame ese desafio que estoy buscando.&nbsp;&nbsp;&nbsp;&nbsp;{"=)"}
             </small>
             <br />
-            <small>
-              Entérate de todas mis proyectos en mi Redes y de paso Seguime :
+            <br />
+            <small className="parrafo ">
+              Entérate de todas mis proyectos en <br />
+              mi Redes y de paso Seguime :
             </small>
-            <div className="gropoIcons">
-              <i class="bi bi-youtube"></i>
-              <i class="bi bi-linkedin"></i>
-              <i class="bi bi-facebook"></i>
-              <i class="bi bi-whatsapp"></i>
+            <div
+              className="row gropoIcons pt-2 pb-2 d-flex
+                        justify-content-center
+                        "
+            >
+              <div className="col-auto">
+                <i class="bi bi-youtube" />
+              </div>
+              <div className="col-auto">
+                <i class="bi bi-linkedin" />
+              </div>
+              <div className="col-auto">
+                <i class="bi bi-whatsapp" />
+              </div>
+              <div className="col-auto">
+                <i class="bi bi-facebook" />
+              </div>
             </div>
           </div>
-          <div className="col-4 bg-dark">asd</div>
-          <div className="col-4 bg-dark">asd</div>
+          <div className="col-auto pt-3  text-center">
+            <div className="row">
+              <div className="col-12 pb-2 ">
+                <h2 className="title-tec">Tecnologías</h2>
+              </div>
+              <div className="col-5 box-color-tec text-start ">
+                <small className="javaScript">&nbsp;&nbsp;</small>
+                &nbsp;<small>JavaScript</small>
+                &nbsp;&nbsp;
+                <i class="bi bi-check2" />
+                <br />
+                <small className="sequelize">&nbsp;&nbsp;</small>
+                &nbsp;<small>Sequelize</small>
+                &nbsp;&nbsp;
+                <i class="bi bi-check2" />
+                <br />
+                <small className="redux">&nbsp;&nbsp;</small>
+                &nbsp;<small>Redux</small>
+                &nbsp;&nbsp;
+                <i class="bi bi-check2" />
+                <br />
+                <small className="react">&nbsp;&nbsp;</small>
+                &nbsp;<small>React</small>
+                &nbsp;&nbsp;
+                <i class="bi bi-check2" />
+                <br />
+                <small className="react-hooks">&nbsp;&nbsp;</small>
+                &nbsp;<small>React Hooks</small>
+                &nbsp;&nbsp;<i class="bi bi-check2"></i>
+                <br />
+                <small className="bootstrap">&nbsp;&nbsp;</small>
+                &nbsp;<small>bootstrap</small>
+                &nbsp;&nbsp;
+                <i class="bi bi-check2" />
+                <br />
+                <small className="posgreSQL">&nbsp;&nbsp;</small>
+                &nbsp;<small>Styled Components</small>
+                &nbsp;&nbsp;
+                <i class="bi bi-check2" />
+                <br />
+              </div>
+              <div className="col-5  box-color-tec text-start">
+                <small className="nodeJs">&nbsp;&nbsp;</small>
+                &nbsp;<small>NodeJs</small>
+                &nbsp;&nbsp;
+                <i class="bi bi-check2" />
+                <br />
+                <small className="css">&nbsp;&nbsp;</small>
+                &nbsp;<small>Css</small>
+                &nbsp;&nbsp;
+                <i class="bi bi-check2" />
+                <br />
+                <small className="html">&nbsp;&nbsp;</small>
+                &nbsp;<small>Html</small>
+                &nbsp;&nbsp;
+                <i class="bi bi-check2" />
+                <br />
+                <small className="posgreSQL">&nbsp;&nbsp;</small>
+                &nbsp;<small>PosgreSQL</small>
+                &nbsp;&nbsp;
+                <i class="bi bi-check2" />
+                <br />
+                <small className="sass">&nbsp;&nbsp;</small>
+                &nbsp;<small>Sass</small>
+                &nbsp;&nbsp;
+                <i class="bi bi-check2" />
+                <br />
+                <small className="aprediendo">&nbsp;&nbsp;</small>
+                &nbsp;<small className="apredis">Aprediendo</small>
+                &nbsp;&nbsp;
+                <i class="bi bi-infinity" />
+                <br />
+              </div>
+            </div>
+          </div>
+          <div className="col mx-2 pt-2">
+            <h3>Estudios realizados</h3>
+            <small>Te invito a que veas mis certificados de estudios</small>
+          </div>
         </div>
         <div className="row">
           <div className="col-12 text-center bg-primary pb-2 pt-2">
